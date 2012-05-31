7409f375b6c3ffa5b6bc141f47562005623e1308
George Oyang <oyanglulu@gmail.com>
2012-05-31 11:09:43 +0800

    Update clearly-highlight.js

--- clearly-highlight.js
+++ clearly-highlight.js
@@ -9,8 +9,19 @@ function loadjs(callback) {
 loadjs(function() {
   $('#readable_iframe').contents().find('head').append('<link rel=\'stylesheet\' type=\'text/css\' href=\'http://yandex.st/highlightjs/6.1/styles/solarized_dark.min.css\' />');
   $.getScript('http://yandex.st/highlightjs/6.1/highlight.min.js', function(){
+    
     $('#readable_iframe').contents().find('pre').each(function() {
-      $(this).replaceWith(function() {
+    var $this = $(this);
+    var ol = $this.find('code ol')    
+    if(ol){
+        ol.find('li').each(function(){
+            $(this).replaceWith(function() {
+            var hl = hljs.highlightAuto($(this).text());
+            return '<li>' + hl.value + '</li>';
+            });
+        });
+    }else
+    $(this).replaceWith(function() {
         var hl = hljs.highlightAuto($(this).text());
         return '<pre><code>' + hl.value + '</code></pre>';
       });
